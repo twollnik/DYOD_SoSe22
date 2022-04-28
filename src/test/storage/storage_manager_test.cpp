f3590f05d510ebbbe3e300752484e323d4feace2
Tom Wollnik <38978338+twollnik@users.noreply.github.com>
2022-04-28 15:50:34 +0200

    Improve test coverage

--- src/test/storage/storage_manager_test.cpp
+++ src/test/storage/storage_manager_test.cpp
@@ -1,4 +1,5 @@
 #include <memory>
+#include <sstream>
 
 #include "../base_test.hpp"
 #include "gtest/gtest.h"
@@ -12,6 +13,7 @@ class StorageStorageManagerTest : public BaseTest {
  protected:
   void SetUp() override {
     auto& storage_manager = StorageManager::get();
+    storage_manager.reset();
     auto table_a = std::make_shared<Table>();
     auto table_b = std::make_shared<Table>(4);
 
@@ -48,6 +50,42 @@ TEST_F(StorageStorageManagerTest, DoesNotHaveTable) {
 TEST_F(StorageStorageManagerTest, HasTable) {
   auto& storage_manager = StorageManager::get();
   EXPECT_EQ(storage_manager.has_table("first_table"), true);
+}
+
+TEST_F(StorageStorageManagerTest, TableNames) {
+  auto& storage_manager = StorageManager::get();
+  EXPECT_EQ(storage_manager.table_names(), (std::vector<std::string>{"first_table","second_table"}));
+}
+
+TEST_F(StorageStorageManagerTest, PrintSimpleTables) {
+  auto& storage_manager = StorageManager::get();
+  std::ostringstream oss;
+  storage_manager.print(oss);
+  EXPECT_EQ(
+    oss.str(), 
+    "=== first_table ===\nn columns: 0\nn rows: 0\nn chunks: 1\ncolumns:\n"
+    "=== second_table ===\nn columns: 0\nn rows: 0\nn chunks: 1\ncolumns:\n"
+  );
+}
+
+TEST_F(StorageStorageManagerTest, PrintMoreComplexTables) {
+  auto& storage_manager = StorageManager::get();
+  std::ostringstream oss;
+  auto first_table = storage_manager.get_table("first_table");
+  first_table->add_column("first_col", "int");
+  first_table->add_column("second_col", "double");
+  first_table->append(std::vector<AllTypeVariant>{AllTypeVariant{1}, AllTypeVariant{2.0}});
+  first_table->append(std::vector<AllTypeVariant>{AllTypeVariant{2}, AllTypeVariant{3.0}});
+  auto second_table = storage_manager.get_table("second_table");
+  second_table->add_column("second_table_first_col", "string");
+  storage_manager.print(oss);
+  EXPECT_EQ(
+    oss.str(), 
+    "=== first_table ===\nn columns: 2\nn rows: 2\nn chunks: 1\ncolumns:\n  "
+    "first_col (int)\n  second_col (double)\n"
+    "=== second_table ===\nn columns: 1\nn rows: 0\nn chunks: 1\ncolumns:\n  "
+    "second_table_first_col (string)\n"
+  );
 }
 
 }  // namespace opossum
